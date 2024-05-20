b7b8c36102b6209c3d36df951816c480aeb7dde2
wwwDayDream <daymonforeman14@gmail.com>
2024-05-20 15:50:36 -0500

    Almost working first version

---
+++ BullwormRifleFF/BullwormRifleFriendlyFire.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using BepInEx;
+using BepInEx.Logging;
+using CessilCellsCeaChells.CeaChore;
+using HarmonyLib;
+
+[assembly: RequiresMethod(typeof(PardnerEquipment_BullwormRifle), nameof(PardnerEquipment_BullwormRifle.Awake), typeof(void))]
+[assembly: RequiresField(typeof(ConnectionPayload), nameof(ConnectionPayload.versionBullworkRifleFF), typeof(string))]
+
+namespace BullwormRifleFF;
+
+[BepInAutoPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public partial class BullwormRifleFriendlyFire : BaseUnityPlugin {
+    public static new ManualLogSource? Logger { get; private set; }
+    public static Harmony Patcher { get; } = new(MyPluginInfo.PLUGIN_GUID); 
+    public void Awake()
+    {
+        Logger = base.Logger;
+
+        Patcher.PatchAll();
+
+        int patchedMethodCount;
+        Logger.LogDebug($"Successfully patched {(patchedMethodCount = Patcher.GetPatchedMethods().Count())} method{(patchedMethodCount == 1 ? "" : "s")}.");
+    }
+}
