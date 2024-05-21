4e7bd3712df63f8ee0234bf4a0e6e052f8bdbf6a
wwwDayDream <daymonforeman14@gmail.com>
2024-05-21 18:04:59 -0500

    BullwormRifleFF Initial commit

--- BullwormRifleFF/BullwormRifleFriendlyFire.cs
+++ BullwormRifleFF/BullwormRifleFriendlyFire.cs
@@ -1,18 +1,22 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
 using CessilCellsCeaChells.CeaChore;
 using HarmonyLib;
-using UnityEngine;
 
-[assembly: RequiresMethod(typeof(PardnerEquipment_BullwormRifle), nameof(PardnerEquipment_BullwormRifle.Awake), typeof(void))]
-[assembly: RequiresField(typeof(ConnectionPayload), nameof(ConnectionPayload.versionBullworkRifleFF), typeof(string), true)]
+[assembly: RequiresMethod(typeof(PardnerEquipment_BullwormRifle), "Awake", typeof(void))]
 
 namespace BullwormRifleFF;
 
+[AttributeUsage(AttributeTargets.Class)]
+internal class MMReqVersion : Attribute {}
+
+[MMReqVersion]
+[BepInDependency("wwwDayDream.MultiplayerModRestrictor")]
 [BepInAutoPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public partial class BullwormRifleFriendlyFire : BaseUnityPlugin {
-    public static new ManualLogSource? Logger { get; private set; }
+    public new static ManualLogSource? Logger { get; private set; }
     public static Harmony Patcher { get; } = new(MyPluginInfo.PLUGIN_GUID); 
     public void Awake()
     {
